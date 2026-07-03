4e282f4ec68b059532ba233c1fbc65e6ec8f9a39
Jatin verma <62111086+jatinverma85276@users.noreply.github.com>
2026-07-03 23:30:27 +0530

    Add: find-missing-and-repeated-values - submission-2

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number[]}
+     */
+    findMissingAndRepeatedValues(grid) {
+        
+        let grid_arr = [];
+        grid = grid.flat().sort((a,b)=>a-b)
+        let grid_set = new Set();
+        for(let i = 0; i<grid.length; i++){
+            if(!grid_set.has(grid[i])){
+                grid_set.add(grid[i])
+            }else{
+                grid_arr.push(grid[i])
+            }
+            
+        }
+        for(let i = 1; i<=grid.length; i++){
+            if(!grid.includes(i)){
+                grid_arr.push(i)
+            }
+        }
+        return grid_arr
+    }
+}
