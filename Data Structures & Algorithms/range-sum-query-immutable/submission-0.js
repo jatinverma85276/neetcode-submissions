5cee1befe36403e06f616673e77628ff9a7575c2
Jatin verma <62111086+jatinverma85276@users.noreply.github.com>
2026-07-05 22:07:19 +0530

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.js
@@ -0,0 +1,21 @@
+class NumArray {
+    /**
+     * @param {number[]} nums
+     */
+    constructor(nums) {
+        this.data = nums
+    }
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        let sum = 0;
+        for(let i = left; i<=right; i++){
+            sum = sum + this.data[i]
+        }
+        return sum
+    }
+}
