3fe9a7e500e0177e5086b9b771e1c2e6b3db366c
Jatin verma <62111086+jatinverma85276@users.noreply.github.com>
2026-07-07 23:19:28 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let newArr = [];
+
+        for(let i = 0; i<arr.length; i++){
+            let max = 0
+            for(let j = i+1; j<arr.length; j++){
+                if(max < arr[j]){
+                    max = arr[j]
+                }
+            }
+            newArr.push(max)
+            if(i === arr.length-1){
+                newArr[arr.length - 1] = -1
+            }
+        }
+        return newArr;
+    }
+}
