c1f5e4d8f611015019f3e3e7eb6b42b6384999ba
Jatin verma <62111086+jatinverma85276@users.noreply.github.com>
2026-07-13 23:10:17 +0530

    Add: string-matching-in-an-array - submission-5

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-5.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {string[]}
+     */
+    stringMatching(words) {
+
+        const result = [];
+
+        for (let i = 0; i < words.length; i++) {
+            for (let j = 0; j < words.length; j++) {
+                if (i !== j && words[j].includes(words[i])) {
+                    result.push(words[i]);
+                    break;  // no need to check further
+                }
+            }
+        }
+
+        return result;
+    }
+}
