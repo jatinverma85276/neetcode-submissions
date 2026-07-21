9f11fcbe04752ccc5a203ec76dc963ff5ac0c182
Jatin verma <62111086+jatinverma85276@users.noreply.github.com>
2026-07-21 21:56:35 +0530

    Add: transpose-matrix - submission-2

---
+++ Data Structures & Algorithms/transpose-matrix/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[][]}
+     */
+    transpose(matrix) {
+        let arr = []
+        for(let i = 0; i<matrix[0].length; i++){
+            let newAr = []
+            for(let j = 0; j<matrix.length; j++){
+                newAr.push(matrix[j][i])
+            }
+            arr.push(newAr)
+
+        }
+
+        return arr
+    }
+}
