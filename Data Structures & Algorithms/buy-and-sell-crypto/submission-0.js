9c45333dfa1969c5f663abd7d08f8e11c8f01ad0
Jatin verma <62111086+jatinverma85276@users.noreply.github.com>
2026-07-22 23:13:29 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let minPrice = prices[0]
+
+        let maxProfit = 0
+        for(let i = 0; i<prices.length; i++){
+
+            if(maxProfit > (prices[i] - minPrice)){
+                maxProfit = maxProfit
+            }else{
+                maxProfit = prices[i] - minPrice
+            }
+
+            if(minPrice < prices[i]){
+                minPrice = minPrice
+            }else{
+                minPrice = prices[i]
+            }
+        }
+
+        return maxProfit
+    }
+}
